3a0a22d1c01d090907356e2f6c0cfa53b038d0e6
Khobs <korbos.int@gmail.com>
2026-04-30 09:08:41 +0200

    remove random spawn locations

--- resources/[qbx]/qbx_spawn/config/client.lua
+++ resources/[qbx]/qbx_spawn/config/client.lua
@@ -1,16 +1,8 @@
 return {
     spawns = {
         {
-            label = 'Legion Square',
-            coords = vec4(195.17, -933.77, 29.7, 144.5)
-        },
-        {
-            label = 'Paleto Bay',
-            coords = vec4(80.35, 6424.12, 31.67, 45.5),
-        },
-        {
-            label = 'Motels',
-            coords = vec4(327.56, -205.08, 53.08, 163.5),
-        },
+            label = 'Lobby',
+            coords = vec4(231.37, -995.15, -95.12, 92.23)
+        }
     }
 }
