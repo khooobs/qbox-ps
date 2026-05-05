81f27f7746660587ffe8e9b5e9d5901dbbc2c497
AbessesWatson <166797590+AbessesWatson@users.noreply.github.com>
2026-05-05 13:16:45 +0200

    Medical
    -Retrait du script medical de qbx
    -Ajout du script medical Abesses d'openspace modifié pour être adapté à PSRP avec le readlog intégré au revive. Ainsi que pour évité les lien inutile ici à d'autre scripts
    - Ajoue dans ox_inventory des items pour Script medical

---
+++ resources/[qbx]/qbx_Ab_Medic_for_PSRP/config.lua
@@ -0,0 +1,21 @@
+Config = {
+
+    office_ui = false, 
+
+    stress_hunger_thrist_activated = false,
+    Ab_AlcoolContrebande_activated = false,
+    Ab_Fatigue__activated = false,
+
+    jobRequired = {'admin'},
+
+    small_heal = 25,
+    mini_health_bandage = 170,
+    max_health = 200,
+
+    revive_item = "medical_kit",
+    fullheal_item = "medical_kit",
+    smallheal_item = 'bandage',
+    check_health_item = 'stethoscope',
+    check_stress_item = 'tensiometre',
+
+}
