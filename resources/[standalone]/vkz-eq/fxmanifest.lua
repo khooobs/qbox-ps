356d0ed2b5b4b1b7582c1290bf67c654881a1fdc
AbessesWatson <166797590+AbessesWatson@users.noreply.github.com>
2026-05-12 15:27:03 +0200

    ajout du script de tremblement de terre de vakzos
    demande de IamYou

---
+++ resources/[standalone]/vkz-eq/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+author "Vkz"
+version '2023-01-30.01'
+
+lua54 'yes'
+
+files {
+    'client/ui/index.html',
+    'client/ui/sounds/earthquake.mp3'
+}
+
+ui_page 'client/ui/index.html'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/core.lua'
+}
+
+server_scripts {
+    'server/core.lua'
+}
